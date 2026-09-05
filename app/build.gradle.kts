30377e211bb4e1750cc4c5148f4b081f6beea398
halumi2701-gif <halumi2701@gmail.com>
2026-09-06 03:29:16 +0700

    Support Android 6+ in native v0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.garsyanimultiusaha.gmuedutrans.erp"
-        minSdk = 26
+        minSdk = 23
         targetSdk = 35
         versionCode = 2
         versionName = "0.2.0-native-test"
