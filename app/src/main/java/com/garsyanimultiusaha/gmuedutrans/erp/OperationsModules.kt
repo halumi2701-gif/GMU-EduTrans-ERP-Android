d86c8195a3bf9f88710210dda187adffb6fcc639
halumi2701-gif <halumi2701@gmail.com>
2026-09-06 09:04:28 +0700

    Fix Trip Control horizontal scrolling

--- app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/OperationsModules.kt
+++ app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/OperationsModules.kt
@@ -1,6 +1,7 @@
 package com.garsyanimultiusaha.gmuedutrans.erp
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
