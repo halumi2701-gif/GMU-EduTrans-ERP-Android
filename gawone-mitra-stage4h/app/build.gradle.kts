590fcc2f839d218364696a6bc952c1db372e110b
halumi2701-gif <halumi2701@gmail.com>
2026-09-09 09:16:13 +0700

    Version GAWONE Mitra Stage 4H

--- gawone-mitra-stage4h/app/build.gradle.kts
+++ gawone-mitra-stage4h/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "site.garsyanimultiusaha.gawone.mitra"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.7.0-stage4g"
+        versionCode = 8
+        versionName = "0.8.0-stage4h"
 
         buildConfigField("String", "SUPABASE_URL", "\"https://fhtxlojbguineyqayhai.supabase.co\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\"sb_publishable_kMYXQocud4kbds7q5fWG-A_Lr-5Xl3h\"")
