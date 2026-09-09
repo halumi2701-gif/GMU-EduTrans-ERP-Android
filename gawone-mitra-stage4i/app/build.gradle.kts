8fc48834e93f18c8b27fe6c1ea39427f03076253
halumi2701-gif <halumi2701@gmail.com>
2026-09-09 09:42:12 +0700

    Use AGP 9 Kotlin compilerOptions DSL

--- gawone-mitra-stage4i/app/build.gradle.kts
+++ gawone-mitra-stage4i/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -83,7 +82,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
 
     packaging {
         resources {
