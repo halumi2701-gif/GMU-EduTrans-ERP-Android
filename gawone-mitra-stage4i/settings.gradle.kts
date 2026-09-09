963e99caa7d6a2fa3ae760e16436393688571bfe
halumi2701-gif <halumi2701@gmail.com>
2026-09-09 09:31:52 +0700

    Rename cumulative project to Stage 4I

--- gawone-mitra-stage4i/settings.gradle.kts
+++ gawone-mitra-stage4i/settings.gradle.kts
@@ -5,5 +5,5 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories { google(); mavenCentral() }
 }
-rootProject.name = "GAWONE-Mitra-Stage4B"
+rootProject.name = "GAWONE-Mitra-Stage4I"
 include(":app")
