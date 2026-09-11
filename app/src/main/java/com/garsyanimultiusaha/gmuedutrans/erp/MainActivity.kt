0e27c9c637c58b8d90c2d1f073a02abd6bf714df
halumi2701-gif <halumi2701@gmail.com>
2026-09-11 19:47:12 +0700

    Enable Notification Rules Preferences on dashboard

--- app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
+++ app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     launchTarget = null
                 }
             }
-            GmuNativeAppWithNotificationDeliveryCenter(vm)
+            GmuNativeAppWithNotificationRulesPreferences(vm)
         }
     }
 
