861d33d1214f7526e0e4d4412ecb6a56dfc9fa9d
halumi2701-gif <halumi2701@gmail.com>
2026-09-11 18:26:29 +0700

    Enable Manager Action Agent wrapper

--- app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
+++ app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContent { GmuNativeAppWithManagerAgent() }
+        setContent { GmuNativeAppWithManagerActionAgent() }
     }
 }
