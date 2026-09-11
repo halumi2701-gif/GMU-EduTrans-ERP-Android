e7cc9b2094e3dfbc292d5d574c9b35b3e26ec265
halumi2701-gif <halumi2701@gmail.com>
2026-09-11 19:39:20 +0700

    Enable Notification Delivery Center on Owner Director dashboard

--- app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
+++ app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     launchTarget = null
                 }
             }
-            GmuNativeAppWithManagerOpsPublishControl(vm)
+            GmuNativeAppWithNotificationDeliveryCenter(vm)
         }
     }
 
