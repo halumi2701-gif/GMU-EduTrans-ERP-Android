f9662c6206a48f8afa75f5ec2beccd425ab7e8df
halumi2701-gif <halumi2701@gmail.com>
2026-09-12 22:57:51 +0700

    Wire Owner Executive Exception Center

--- app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
+++ app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     launchTarget = null
                 }
             }
-            GmuNativeAppWithApprovalAuthority(vm)
+            GmuNativeAppWithOwnerExecutiveExceptions(vm)
         }
     }
 
