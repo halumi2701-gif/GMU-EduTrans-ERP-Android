4905e8bbde6a2244fa714bffe0401aa616c9dbe6
halumi2701-gif <halumi2701@gmail.com>
2026-09-12 23:09:55 +0700

    Wire Executive Action Queue into app

--- app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
+++ app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     launchTarget = null
                 }
             }
-            GmuNativeAppWithOwnerExecutiveExceptions(vm)
+            GmuNativeAppWithExecutiveActionQueue(vm)
         }
     }
 
