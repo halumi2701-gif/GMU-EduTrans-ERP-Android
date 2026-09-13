f28b958e61ea387c832c31e8492e7e5a84fee7cf
halumi2701-gif <halumi2701@gmail.com>
2026-09-13 20:08:33 +0700

    wire corrective review calendar

--- app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
+++ app/src/main/java/com/garsyanimultiusaha/gmuedutrans/erp/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     launchTarget = null
                 }
             }
-            GmuNativeAppWithExecutiveCorrectiveActionPlan(vm)
+            GmuNativeAppWithCorrectivePlanReviewCalendar(vm)
         }
     }
 
