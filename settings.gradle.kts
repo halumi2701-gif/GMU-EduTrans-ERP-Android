bd396778065838248a8a8724c63bdc4a529a5656
halumi2701-gif <halumi2701@gmail.com>
2026-09-16 16:52:41 +0700

    feat(tourleader): add standalone GMU Tour Leader Android app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,3 +14,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "GMUEduTransERP"
 include(":app")
+include(":tourleader")
---
+++ tourleader/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.garsyanimultiusaha.gmuedutrans.tourleader"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.garsyanimultiusaha.gmuedutrans.tourleader"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
