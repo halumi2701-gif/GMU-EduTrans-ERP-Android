63780d8c522a61483832f3b119c6834111c323c2
halumi2701-gif <halumi2701@gmail.com>
2026-09-17 20:01:34 +0700

    Launch Sales v6.4 with runtime E2E health guard

--- sales-app/src/main/java/com/garsyanimultiusaha/gmuedutrans/sales/MainActivity.kt
+++ sales-app/src/main/java/com/garsyanimultiusaha/gmuedutrans/sales/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val vm: SalesViewModel = viewModel()
-            SalesAppV63(vm)
+            SalesAppV64(vm)
         }
     }
 }
